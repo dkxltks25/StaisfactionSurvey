94e344eb9d3fba8945e20222192ee2e95e7a9ca8
dkxltks25 <dkxltks25@naver.com>
2020-01-03 17:51:32 +0900

    function not change

--- Survey/Survey/AdminManagementPage.xaml.cs
+++ Survey/Survey/AdminManagementPage.xaml.cs
@@ -15,6 +15,8 @@ using Sql = Survey.ManageSql;
 using Survey.ViewModel;
 using static Survey.ViewModel.AdminViewModel;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
+
 namespace Survey
 {
     /// <summary>
@@ -22,48 +24,28 @@ namespace Survey
     /// </summary>
     public partial class AdminManagementPage : Window
     {
-        public AdminViewModel Vm = new ViewModel.AdminViewModel();
-        public ObservableCollection<AdminList> list;
+        /// <summary>
+        /// BtnState 
+        /// 0 입력
+        /// 1 수정
+        /// 
+        /// </summary>
+        private int BtnState = 0;
+
+        //바인딩 리스트 정의
+        BindingList<AdminViewModel> myViewModel;
         public AdminManagementPage()
         {
             InitializeComponent();
-
-            this.DataContext = Vm;
+            myViewModel = new BindingList<AdminViewModel>();
+            DG1.ItemsSource = myViewModel;
+            AdminInfo.DataContext = new AdminViewModel();
             DefaultButtonState();
         }
-
-        
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-        }
-
-        private void ListView1_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
-        }
-
-        private void StudentNumber_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-        }
-
-        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
-        }
-
-        private void ComboBoxItem_Selected(object sender, RoutedEventArgs e)
-        {
-
-        }
-        //하단 메세지 창
-        private void DockMessageStateChange(String Message)
-        {
-
-        }
-
+        //***************************************************
         //normal
+        //***************************************************
+
         private void DefaultButtonState()
         {
             InfoButton.IsEnabled = true;
@@ -73,7 +55,10 @@ namespace Survey
             CheckButton.IsEnabled = false;
             CancelButton.IsEnabled = false;
         }
+        //***************************************************
         //Add, update ,Cancel을 누른 후
+        //***************************************************
+
         private void DataChangeButtonState()
         {
             InfoButton.IsEnabled = true;
@@ -82,91 +67,130 @@ namespace Survey
             DeleteButton.IsEnabled = false;
             CheckButton.IsEnabled = true;
             CancelButton.IsEnabled = true;
+            
         }
-        //데이터 클리후
-        private void UpdateAndAddButtonState()
+        #region 입력 창 reset
+        private void reset()
+        {
+            AdminViewModel data = (AdminViewModel)AdminInfo.DataContext;
+            data.AdminId = "";
+            data.AdminName = "";
+            data.AdminPassword = "";
+            data.AdminSGrade = "M";
+        }
+        #endregion
+        //***************************************************
+        //데이터 클리어 후
+        //***************************************************
+
+        private void UpdateAndDeleteButtonState()
         {
             InfoButton.IsEnabled = true;
             AddButton.IsEnabled = false;
             UpdateButton.IsEnabled = true;
             DeleteButton.IsEnabled = true;
             CheckButton.IsEnabled = false;
-            CancelButton.IsEnabled = false;
+            CancelButton.IsEnabled = true;
         }
-        
+        //***************************************************
         //info 버튼
+        //***************************************************
         private void InfoButton_Click(object sender, RoutedEventArgs e)
         {
 
         }
+        //***************************************************
         //Add 버튼
+        //***************************************************
         private void AddButton_Click(object sender, RoutedEventArgs e)
         {
-            //DataChangeButtonState();
-            Sql sql = new Sql();
-            Console.WriteLine(sql.SHA512("dkxltks25"));
+           
+            reset();
+            DataChangeButtonState();
+            //Sql sql = new Sql();
+            //Console.WriteLine(sql.SHA512("dkxltks25"));
 
         }
+
+        //***************************************************
         //Update 버튼 클릭
+        //***************************************************
         private void UpdateButton_Click(object sender, RoutedEventArgs e)
         {
-            DataChangeButtonState();
+            BtnState = 1;
+            Console.WriteLine(DG1.SelectedIndex);
+            Console.WriteLine(myViewModel[DG1.SelectedIndex].AdminId);
+            myViewModel[DG1.SelectedIndex].AdminCode = "S";
+            //AdminViewModel Data = (AdminViewModel)AdminInfo.DataContext;
+
+            //Data.AdminCode = "S";
+            //reset();
+            DefaultButtonState();
 
         }
+        //***************************************************
         //Cancel 버튼 클릭
+        //***************************************************
         private void DeleteButton_Click(object sender, RoutedEventArgs e)
         {
             DataChangeButtonState();
 
         }
+        //***************************************************
         //Check 버튼 클릭
+        //***************************************************
         private void CheckButton_Click(object sender, RoutedEventArgs e)
         {
+            if(BtnState == 0)
+            {
+                int ItemCount = DG1.Items.Count;
+                if (ItemCount < 0)
+                {
+                    ItemCount = 0;
+                }
+
+                AdminViewModel data = (AdminViewModel)AdminInfo.DataContext;
+                string AdminCode = "A";
+                AdminViewModel TempData = new AdminViewModel
+                {
+                    AdminCode = AdminCode,
+                    AdminId = data.AdminId,
+                    AdminName = data.AdminName,
+                    AdminPassword = data.AdminPassword,
+                    AdminSGrade = data.AdminSGrade
+                };
+                myViewModel.Insert(ItemCount, TempData);
+                
+            }
             DefaultButtonState();
+            reset();
+
         }
+
         //Cancel 버튼 클릭
         private void CancelButton_Click(object sender, RoutedEventArgs e)
         {
+            reset();
             DefaultButtonState();
         }
 
-        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
-        {
-
-        }
+    
 
         private void DG1_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-
+            
+            DataGrid Select = (DataGrid)sender;
+            AdminInfo.DataContext = myViewModel[Select.SelectedIndex];
+            UpdateAndDeleteButtonState();
         }
 
-        private void DG1_TouchEnter(object sender, TouchEventArgs e)
-        {
-        }
+     
 
         private void DG1_SelectedCellsChanged(object sender, SelectedCellsChangedEventArgs e)
         {
-            DataGrid dg = sender as DataGrid;
-            Console.WriteLine(dg.Items.Count);
-            try
-            {
-                AdminList row = (AdminList)dg.SelectedItems[0];
-                Vm.AdminId = row.AdminId;
-                Vm.AdminName = row.AdminName;
-                Vm.AdminPassword = row.AdminPassword;
-                Vm.AdminSGrade = row.AdminSGrade;
-            }
-            catch (Exception e1)
-            {
-            }
             
-            
-
-
         }
 
-        private void DG1_ColumnReordering(object sender, DataGridColumnReorderingEventArgs e)
-        {
-        }
+      
     }
 }
--- Survey/Survey/ViewModel/AdminViewModel.cs
+++ Survey/Survey/ViewModel/AdminViewModel.cs
@@ -10,77 +10,31 @@ using System.Globalization;
 namespace Survey.ViewModel
 {
     public class AdminViewModel : INotifyPropertyChanged
-        {
+    {
         #region 생성자
         public AdminViewModel()
         {
             //Add커맨드 할당
-            AddCommand = new Command.Command(ExecuteAdd, CanExecuteAdd);
-        }
-        #endregion 데이터 그리드 뷰 정보
-        public class AdminList
-        {
-            public string AdminCode{ get; set; }
-            public string AdminId { get; set; }
-            public string AdminPassword{ get; set; }
-            public string AdminName{ get; set; }
-            public string AdminSGrade{ get; set; }
-            public string AdminCreateAt { get; set; }
-            public int GetAllSize()
-            {
-                return AdminCode.Length + AdminId.Length + AdminPassword.Length + AdminName.Length + AdminSGrade.Length + AdminCreateAt.Length;
-            }
-        }
-
-        #region ICommand 제어
-        public ICommand AddCommand { get; set; }
-        //Add Command
-        private void ExecuteAdd(object obj)
-        {
-            //리스트 정보
-            AdminList adl = new AdminList();
-            Console.WriteLine(adl);
-            adl.AdminCode = "A";
-            adl.AdminId =  AdminId;
-            adl.AdminPassword = AdminPassword;
-            adl.AdminName = AdminName;
-            adl.AdminSGrade = AdminSGrade;
-
-            AdminLists.Add(adl);
-            AddEnabled = false;
-            
-        }
-        private bool CanExecuteAdd(object args)
-        {
-            return true;
-        }
-        private Boolean _AddEnabled =false;
-        public Boolean AddEnabled
-        {
-            get { return _AddEnabled; }
-            set
-            {
-                _AddEnabled = value;
-                NotifyPropertyChanged("AddEnabled");
-            }
+            //AddCommand = new Command.Command(ExecuteAdd, CanExecuteAdd);
         }
         #endregion
 
-        #region 데이터그리드 뷰
-        private ObservableCollection<AdminList> _AdminLists = new ObservableCollection<AdminList>();
-        public ObservableCollection<AdminList> AdminLists
-        {
-            get { return _AdminLists; }
-            set { _AdminLists = value; NotifyPropertyChanged("AdminLists"); }
-        }
-        public void RemoveList (int Position)
-        {
-            Console.WriteLine(AdminLists.Count);
-            AdminLists.RemoveAt(Position);
-        }
-        #endregion
+
 
         #region Admin 기본정보
+        private string Code;
+        public string AdminCode
+        {
+            get
+            {
+                return Code;
+            }
+            set
+            {
+                Code = value;
+                NotifyPropertyChanged(Code);
+            }
+        }
         private string Id;
         public string AdminId
         {
@@ -89,7 +43,7 @@ namespace Survey.ViewModel
                 return Id;
             }
             set {
-                if(value != null)
+                if (value != null)
                 {
                     Id = value;
                     NotifyPropertyChanged("AdminId");
@@ -129,7 +83,7 @@ namespace Survey.ViewModel
             }
         }
 
-       
+
         private string SGrade;
         public string AdminSGrade
         {
@@ -146,16 +100,50 @@ namespace Survey.ViewModel
         }
         #endregion
         public event PropertyChangedEventHandler PropertyChanged;
-
-
         //이벤트 할당
         protected void NotifyPropertyChanged(string PropertyName)
         {
-            if(PropertyChanged != null)
+            if (PropertyChanged != null)
             {
                 PropertyChanged(this, new PropertyChangedEventArgs(PropertyName));
             }
         }
-       
+
+        #region ICommand 제어
+        public ICommand AddCommand { get; set; }
+        //Add Command
+        /*
+         private void ExecuteAdd(object obj)
+        {
+            //리스트 정보
+            AdminList adl = new AdminList();
+            Console.WriteLine(adl);
+            adl.AdminCode = "A";
+            adl.AdminId = AdminId;
+            adl.AdminPassword = AdminPassword;
+            adl.AdminName = AdminName;
+            adl.AdminSGrade = AdminSGrade;
+
+            AdminLists.Add(adl);
+            AddEnabled = false;
+
+        }
+        */
+        private bool CanExecuteAdd(object args)
+        {
+            return true;    
+        }
+        private Boolean _AddEnabled = false;
+        public Boolean AddEnabled
+        {
+            get { return _AddEnabled; }
+            set
+            {
+                _AddEnabled = value;
+                NotifyPropertyChanged("AddEnabled");
+            }
+        }
+        #endregion
+
     }
 }
